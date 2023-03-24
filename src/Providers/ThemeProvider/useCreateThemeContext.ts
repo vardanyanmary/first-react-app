6db7538352e34fecbaf360bb36cb214d1bf68928
Mary <mary.vardanyan.2001@gmail.com>
2023-03-24 19:04:17 +0400

    Theme provider

--- src/Providers/ThemeProvider/useCreateThemeContext.ts
+++ src/Providers/ThemeProvider/useCreateThemeContext.ts
@@ -5,6 +5,10 @@ export type Theme = 'dark' | 'light'
 function useCreateThemeContext() {
 	const [theme, setTheme] = useState<Theme>('dark');
 
+    // theme changes hook , petq chi amen angam nuyn texy sarqel,
+    // asenq themeSwitcheri mej , menq karanq iran mi angam hooki 
+    // mej sarqenq u asenq guyny poxi 
+
     const toggleTheme = () => setTheme(
         prev => prev === 'light'
           ? 'dark'
